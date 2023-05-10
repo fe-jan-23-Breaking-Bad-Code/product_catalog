aa176dde27a2203b8269643c19018e3a03ac477c
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-05-10 22:44:51 +0300

    Cart page v3 and footer fixed

--- src/pages/CartPage/CartList.tsx
+++ src/pages/CartPage/CartList.tsx
@@ -3,7 +3,11 @@ import CartList from './CartList';
 import { CartItem } from '../../components/Cart/CartItem/CartItem';
 
 export const CartPage: React.FC = () => (
-  <CartItem />
+  <>
+    <CartItem />
+    <CartItem />
+    <CartItem />
+  </>
 );
 
 export default CartPage;
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,32 +1,51 @@
 import React from 'react';
 import CartList from './CartList';
 import styles from './CartPage.module.scss';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import Vector from '../../img/vector-left.svg';
 
-export const CartPage: React.FC = () => (
-  <>
-    <div className={`${styles.container} ${styles.grid} ${styles['grid--desktop']} ${styles['grid--tablet']}`}>
-      <div className={`${styles['cart-list']} ${styles['grid__item--desktop-1-16']}`}>
-        <h1 className={`${styles.title}`}>
-          Cart
-        </h1>
+export const CartPage: React.FC = () => {
+  const navigate = useNavigate();
 
-        <CartList />
-      </div>
+  const handleCheckoutClick = () => {
+    navigate('/');
+  };
 
-      <div className={`${styles['checkout-block']} ${styles['grid__item--desktop-17-24']}`}>
-        <div className={styles['checkout-block__info']}>
-          <p className={styles['checkout-block__cost']}>
-            $2567
+  return (
+    <>
+      <div className={`${styles.container} ${styles.grid} ${styles['grid--desktop']} ${styles['grid--tablet']}`}>
+        <p className={styles.link} onClick={() => window.history.go(-1)}>
+          <img className={styles.vector} src={Vector} alt="Back" />
+          <p>
+            Back
           </p>
-          <p className={styles['checkout-block__items-count']}>
-            Total for 3 items
-          </p>
+        </p>
+        <div className={`${styles['cart-list']} ${styles['grid__item--desktop-1-16']}`}>
+          <h1 className={`${styles.title}`}>
+            Cart
+          </h1>
+
+          <CartList />
         </div>
-        <button className={styles['checkout-block__button']}>Checkout</button>
+
+        <div className={`${styles['checkout-block']} ${styles['grid__item--desktop-17-24']}`}>
+          <div className={styles['checkout-block__info']}>
+            <p className={styles['checkout-block__cost']}>
+              $2567
+            </p>
+            <p className={styles['checkout-block__items-count']}>
+              Total for 3 items
+            </p>
+          </div>
+          <button
+            className={styles['checkout-block__button']}
+            onClick={handleCheckoutClick}>
+              Checkout
+          </button>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default CartPage;
