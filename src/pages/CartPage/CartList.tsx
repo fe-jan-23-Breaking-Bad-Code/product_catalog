e1c03c0b2c7918a18f05518903e43b3f91aa9a61
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-05-10 23:49:07 +0300

    Calculate cost imlement

--- src/pages/CartPage/CartList.tsx
+++ src/pages/CartPage/CartList.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import CartList from './CartList';
 import { CartItem } from '../../components/Cart/CartItem/CartItem';
+import styles from './CartPage.module.scss';
 
-export const CartPage: React.FC = () => (
-  <>
-    <CartItem />
-    <CartItem />
-    <CartItem />
-  </>
-);
+interface CartItem {
+  id: string,
+  count: number,
+}
 
-export default CartPage;
+type Props = {
+  cart: CartItem[];
+};
+
+export const CartList: React.FC<Props> = ({ cart }) => {
+  return (
+    <div>
+      {cart.map((cartItem) => {
+        return (
+          <div key={cartItem.id} className={styles.cart__item}>
+            <CartItem />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CartList;
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -3,8 +3,19 @@ import CartList from './CartList';
 import styles from './CartPage.module.scss';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import Vector from '../../img/vector-left.svg';
+import { useAppSelector } from '../../hooks';
+
+interface CartItem {
+  id: string,
+  count: number,
+}
+
+const checkoutCost = (cart: CartItem[]): number => {
+  return cart.reduce((total, item) => total + item.count, 0);
+};
 
 export const CartPage: React.FC = () => {
+  const cart = useAppSelector(store => store.cart);
   const navigate = useNavigate();
 
   const handleCheckoutClick = () => {
@@ -25,16 +36,16 @@ export const CartPage: React.FC = () => {
             Cart
           </h1>
 
-          <CartList />
+          <CartList cart={cart} />
         </div>
 
         <div className={`${styles['checkout-block']} ${styles['grid__item--desktop-17-24']}`}>
           <div className={styles['checkout-block__info']}>
             <p className={styles['checkout-block__cost']}>
-              $2567
+              {`$${checkoutCost(cart)}`}
             </p>
             <p className={styles['checkout-block__items-count']}>
-              Total for 3 items
+              {`Total for ${cart.length} items`}
             </p>
           </div>
           <button
