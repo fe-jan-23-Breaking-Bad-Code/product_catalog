cc4f5b5fe73e4abcdd68bd7c7db7a5c278b792b4
Sasha Dranuta <dranuta97@gmail.com>
2023-05-16 20:02:18 +0300

    Props implemented

--- src/pages/OrdersPage/OrdersPage.tsx
+++ src/pages/OrdersPage/OrdersPage.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './OrdersPage.module.scss';
 import { BackButton } from '../../components/BackButton/BackButton';
 import { PagesTitle } from '../../components/PagesTitle/PagesTitle';
 import { OrderList } from './OrderList';
+import { getOrders } from '../../API/FetchPhones';
+import { Orders } from '../../types/Orders';
 
 export const OrdersPage: React.FC = () => {
+  const [orders, setOrders] = useState<Orders[]>([]);
+
+  const getOrdersFromServer = async () => {
+    try {
+      const ordersFromServer = await getOrders();
+
+      setOrders(ordersFromServer);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getOrdersFromServer();
+  }, []);
+
   return (
     <>
       <div className={`${styles.container} ${styles.grid} ${styles['grid--desktop']}`}>
@@ -14,7 +32,7 @@ export const OrdersPage: React.FC = () => {
           <div className={styles.title}>
             <PagesTitle title="Order list" />
 
-            <OrderList />
+            <OrderList orders={orders} />
           </div>
         </div>
       </div>
